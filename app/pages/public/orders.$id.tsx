01a881124fac029e183069139800885cc72378a3
Usmaarn <Usmaarn@gmail.com>
2025-05-16 11:25:29 +0100

    first commit

--- app/pages/public/orders.$id.tsx
+++ app/pages/public/orders.$id.tsx
@@ -1,15 +1,10 @@
 import { orderService } from "~/services/order-service";
 import type { Route } from "./+types";
-import { paymentService } from "~/services/payment-service";
+import { paymentService, type OrderItem } from "~/services/payment-service";
 import { data } from "react-router";
-import { Input, Tag } from "antd";
-import { calculateDiscount } from "~/lib/utils";
+import { Avatar, Card, Col, Input, List, Row, Space, Tag } from "antd";
+import { calculateDiscount, getImageSrc } from "~/lib/utils";
 import { OrderStatus } from "~/lib/enums";
-import {
-  ProductImage,
-  ProductProvider,
-  ProductTitle,
-} from "~/components/product";
 import { PriceFormat } from "~/components/price-format";
 
 export async function loader({ params }: Route.LoaderArgs) {
@@ -17,7 +12,6 @@ export async function loader({ params }: Route.LoaderArgs) {
   if (order) {
     const response = await paymentService.verifyOrderPayment(order);
     if (response?.success) {
-      console.log(response?.data);
       return { order: response.data };
     }
     return { order: { ...order, status: 0 } };
@@ -57,79 +51,81 @@ export default function Page({ loaderData: { order } }: Route.ComponentProps) {
   }
 
   return (
-    <div className="p-3 max-w-6xl mx-auto grid md:grid-cols-3 gap-8">
-      <h2 className="md:col-span-3 text-2xl font-bold">Order Details</h2>
-      <div className="space-y-5 md:col-span-2">
-        <div className="border rounded-2xl bg-gray-50">
-          <h3 className="font-bold px-5 py-3 border-b">Order Items</h3>
-          <div className="px-5 py-3 space-y-2">
-            {order.items.map((item: any, index: number) => (
-              <ProductProvider key={index} product={item.product}>
-                <div
-                  key={item.product.id}
-                  className="border bg-gray-100 rounded-2xl p-2 flex gap-3"
-                >
-                  <div className="">
-                    <ProductImage size={50} />
-                  </div>
-                  <div className="text-sm space-y-1">
-                    <h3>
-                      <ProductTitle />
-                    </h3>
-                    <p>QTY: {item.quantity}</p>
-                  </div>
-                </div>
-              </ProductProvider>
-            ))}
-          </div>
-        </div>
-
-        <div className="border rounded-2xl bg-gray-50">
-          <h3 className="font-bold px-5 py-3 border-b">Delivery Address</h3>
-          <div className="px-5 py-3 space-y-2">
+    <Row gutter={[24, 24]} className="p-3 max-w-6xl! mx-auto!">
+      <Col span={24}>
+        <h2 className="md:col-span-3 text-2xl font-bold">Order Details</h2>
+      </Col>
+      <Col span={24} md={16}>
+        <Space direction="vertical" className="w-full!">
+          <Card
+            title="Order Items"
+            className="bg-gray-50!"
+            classNames={{ body: "p-0!" }}
+          >
+            <List
+              size="small"
+              dataSource={order.items}
+              renderItem={(item: OrderItem, index) => (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar={
+                      <Avatar
+                        src={getImageSrc(item.product.images[0])}
+                        shape="square"
+                        size="large"
+                      />
+                    }
+                    title={item.product.name}
+                    description={<p>QTY: {item.quantity}</p>}
+                  />
+                </List.Item>
+              )}
+            />
+          </Card>
+          <Card title="Delivery Address" className="bg-gray-50!">
             <Input.TextArea
               placeholder="Enter delivery address"
               readOnly
               disabled
               value={order.deliveryAddress!}
               name="address"
-              className="bg-white"
             />
+          </Card>
+        </Space>
+      </Col>
+
+      <Col span={24} md={8}>
+        <Card
+          title="Order Summary"
+          className="bg-gray-50!"
+          classNames={{ body: "space-y-2" }}
+        >
+          <div className="flex justify-between">
+            <p>
+              {"Item's Total"} ({totalItems})
+            </p>
+            <PriceFormat value={totalAmount} />
           </div>
-        </div>
-      </div>
-
-      <div className="">
-        <div className="border rounded-2xl bg-gray-50">
-          <h3 className="font-bold px-5 py-3 border-b">Order Summary</h3>
-          <div className="px-5 py-3 space-y-2">
-            <div className="flex justify-between">
-              <p>
-                {"Item's Total"} ({totalItems})
-              </p>
-              <PriceFormat value={totalAmount} />
-            </div>
-            <div className="flex justify-between">
-              <p>Delivery Fee</p>
-              <PriceFormat value={deliveryFee} />
-            </div>
-            <div className="flex justify-between">
-              <p>Discount({discountPercentage.toFixed(1)}%)</p>
-              <PriceFormat value={discountAmount} />
-            </div>
-
-            <div className="flex justify-between border-t pt-3 font-bold">
-              <p>Total</p>
-              <PriceFormat value={totalAmount - discountAmount} />
-            </div>
-
-            <div className="flex justify-between border-t pt-3">
-              <p className="font-bold">Status</p>
-              <p className="font-bold">{displayStatus(order.status!)}</p>
-            </div>
+          <div className="flex justify-between">
+            <p>Delivery Fee</p>
+            <PriceFormat value={deliveryFee} />
           </div>
-        </div>
-      </div>
-    </div>
+          <div className="flex justify-between">
+            <p>Discount({discountPercentage.toFixed(1)}%)</p>
+            <PriceFormat value={discountAmount} />
+          </div>
+
+          <div className="flex justify-between border-t pt-3 font-bold">
+            <p>Total</p>
+            <PriceFormat value={totalAmount - discountAmount} />
+          </div>
+
+          <div className="flex justify-between border-t pt-3">
+            <p className="font-bold">Status</p>
+            <p className="font-bold">{displayStatus(order.status!)}</p>
+          </div>
+        </Card>
+      </Col>
+    </Row>
   );
 }
